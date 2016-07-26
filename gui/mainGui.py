a6a63ccad5820927a91162d3f555dd4eb6ca50aa
mastraa <mastraa@gmail.com>
2016-07-26 20:55:52 +0200

    gui implementation
    begin to write gui code

---
+++ gui/mainGui.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 26 13:58:33 2016
+
+@author: mastraa
+"""
+
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+
+def prova():
+    plt.plot([0,1,2],[1,2,3])
+    plt.show()
+
+class MainWindow(tk.Tk):
+    """
+    """
+    def __init__(self, title, size):
+        tk.Tk.__init__(self)
+        self.title(title)
+        self.geometry(size)#x,y
+        self.resizable(0,0)
+        self.configure(background="gray89")
+        
+        master = ttk.Frame(self, name='master') # create Frame in self
+        master.pack(fill=tk.BOTH) # fill both sides of the parent
+          
+        noteBook = ttk.Notebook(master)
+        p1 = ttk.Frame(noteBook)   # first page, which would get widgets gridded into it
+        p2 = ttk.Frame(noteBook)   # second page
+        p3 = ttk.Frame(noteBook)
+        noteBook.add(p1, text='Load Data')
+        noteBook.add(p2, text='Material Data')
+        noteBook.add(p3, text='Analysis')
+        noteBook.pack(fill=tk.BOTH, padx=2, pady=3) # fill "master" but pad sides
+          
+          
+        """ Load Setting Page """
+        sec1=ttk.LabelFrame(p1,text="File setting")
+        sec1.grid(column=0,row=0,sticky='W', padx=10, pady=10)
+        ttk.Label(sec1,text="type").grid(column=0,row=0)
+        ttk.Label(sec1,text="url").grid(column=0,row=1)
+        
+         
+        sec2=ttk.LabelFrame(p1,text="Counting Method")
+        sec2.grid(column=0,row=1,sticky='W', padx=10, pady=10)
+        ttk.Label(sec2,text="method").grid(column=0,row=0)
+        ttk.Label(sec2,text="delta").grid(column=0,row=1)
+          
+        plotter=ttk.LabelFrame(p1,text="View data")
+        plotter.grid(column=1,row=0,rowspan=2, sticky='NW', padx=10, pady=10)
+        ttk.Button(plotter, text="ciao", command=prova).grid(column=0,row=0)
+          
+         
+        """Material data page"""
+          
+          
+        """Analysis page"""
+          
+          
+radice=MainWindow("Titolo","900x600")
+radice.mainloop()
